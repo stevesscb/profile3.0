0fa1d34f49d9b19af9812bd0a1c4ae52fd1f7ed9
Stephen <stevesscb@gmail.com>
2024-02-20 15:50:54 -0500

    add favicon, code cleanup

--- src/layout/pages/home/Home.jsx
+++ src/layout/pages/home/Home.jsx
@@ -1,16 +1,17 @@
+import { Link } from 'react-router-dom';
+
 import MotionAnimate from 'src/components/MotionAnimate';
 
 import classes from './home.module.scss';
-import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className={classes.home}>
       <MotionAnimate>
-        <h2>My passion is to</h2>
-        <h3>build,</h3>
-        <h3>optimize,</h3>
-        <h3>& deliver websites & applications.</h3>
+        <p>My passion is to</p>
+        <p>build,</p>
+        <p>optimize,</p>
+        <p>& deliver websites & applications.</p>
         <span className={classes.cta}>
           <Link to='projects'>My work</Link>
         </span>
