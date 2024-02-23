7ca373e839d705494fedf3144a970aa758fa494a
Stephen <stevesscb@gmail.com>
2024-02-23 13:58:20 -0500

    add categories to projects, add portfolio project

--- src/projectData.js
+++ src/projectData.js
@@ -1,6 +1,6 @@
-export const projects = [
+export const professionalProjects = [
   {
-    id: 'panel1',
+    id: 'professional1',
     title: 'Horizon Digital',
     description:
       'For this project I was faced with the challenge of designing and building a new website for the company I work for. I learnt a lot of valuable skills including best design practices for improved UX experience and accessibility standards.',
@@ -8,23 +8,23 @@ export const projects = [
   },
 
   {
-    id: 'panel2',
+    id: 'professional2',
     title: 'Jobs In Cyber',
     description:
-      'This task was to give an existing web application a revamp. Carrying out bug fixes, implementing new features and building new designs. It was great to be a part of such a collaborative process.',
-    url: 'https://uat.jobsincyber.com/candidate/listings',
+      'This task was to give a web application a revamp. Carrying out bug fixes, implementing new features and building new designs. It was great to be a part of such a collaborative process.',
+    url: '#',
   },
 
   {
-    id: 'panel3',
+    id: 'professional3',
     title: 'Lingnan University',
     description:
-      'During my time working as an intern in a web agency in Hong Kong I had the opportunity to work on a complete custom WordPress theme build. Once the UI was designed I had to map out exactly what data would be required and then categorize this into taxonomies and custom post types. Once we were happy with the backend solution I then moved into development where I gained valuable experience in writing clean, efficient code, producing my first ever professional website.',
+      'During my time working as an intern in a web agency in Hong Kong I had the opportunity to work on a complete custom WordPress theme build. Once the UI was designed I had to map out exactly what data would be required and then categorize this into taxonomies and custom post types. Once we were happy with the backend solution I then moved into development where I gained valuable experience in writing clean, efficient code.',
     url: 'https://www.ln.edu.hk/cultural/',
   },
 
   {
-    id: 'panel4',
+    id: 'professional4',
     title: 'Hiremii',
     description:
       'In this project I teamed up with two backend developers to create an app for Hiremii. This app was built for the recruitment industry using AI to shortlist potential candidates to make the hiring process more efficient. It was my job to build the designs and collaborate with the team. I was also involved in dealing with the client directly, discussing designs and best practices. I gained skills in communication and learnt the importance of git flow.',
@@ -32,7 +32,7 @@ export const projects = [
   },
 
   {
-    id: 'panel5',
+    id: 'professional5',
     title: 'Enable',
     description:
       'This application needed a landing page, so I designed and built a template for the company to insert their own copy into. Working alone on this project gave me some flexibility to choose my own tech and recourses. For this task I ended up using Gatsby for its fast light weight framework. I enjoy using React in projects as I find the approach of breaking down code into small reusable components efficient and clean.',
@@ -40,10 +40,20 @@ export const projects = [
   },
 
   {
-    id: 'panel6',
+    id: 'professional6',
     title: 'My Foodie Box',
     description:
       'This project was to freshen up an existing website by building a new design and also implementing a mobile friendly version.',
     url: 'https://myfoodiebox.com.au/',
+  },
+];
+
+export const personalProjects = [
+  {
+    id: 'personal1',
+    title: 'Portfolio site',
+    description:
+      'For this assignment, I wanted to create my own portfolio website utilizing CSS grid and SCSS along with React Gatsby. I chose to use SCSS as I enjoy using the BEM notation in my HTML - I find this method very clean and clear to read. I also enjoyed steering away from using any CSS framework as I was able to create all my own layouts and components relying on my skills as a CSS developer',
+    url: 'https://scb-development.vercel.app/',
   },
 ];
