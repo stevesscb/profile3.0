f811668110304d6c213daf2153304b525fd6599f
Stephen <stevesscb@gmail.com>
2024-06-14 07:30:59 +0100

    fixed spelling

--- src/projectData.js
+++ src/projectData.js
@@ -1,56 +1,56 @@
 export const professionalProjects = [
   // Horizon Digital
   {
-    id: 'professional1',
-    title: 'Horizon Digital',
+    id: "professional1",
+    title: "Horizon Digital",
     description:
-      'In this project I designed and built a new company website. I applied skills including good design practices for improved UX, accessibility and SEO optimization in collaboration with others.',
-    url: 'https://horizondigital.au/',
+      "In this project I designed and built a new company website. I applied skills including good design practices for improved UX, accessibility and SEO optimization in collaboration with others.",
+    url: "https://horizondigital.au/",
   },
 
   // Hiremii
   {
-    id: 'professional4',
-    title: 'Hiremii',
+    id: "professional4",
+    title: "Hiremii",
     description:
-      'Teaming up with two backend developers, I worked on creating an app for Hiremii. This app was built for the recruitment industry using AI to shortlist potential candidates to make the hiring process more efficient. It was my job to build the designs and collaborate with the team. I was also involved in working with the client directly, discussing designs and best practices.',
-    url: 'https://hiremii.com/',
+      "Teaming up with two backend developers, I worked on creating an app for Hiremii. This app was built for the recruitment industry using AI to shortlist potential candidates to make the hiring process more efficient. It was my job to build the designs and collaborate with the team. I was also involved in working with the client directly, discussing designs and best practices.",
+    url: "https://hiremii.com/",
   },
 
   // Lingnan University
   {
-    id: 'professional3',
-    title: 'Lingnan University',
+    id: "professional3",
+    title: "Lingnan University",
     description:
-      'This project involved building a custom WordPress theme from scratch. Once the UI was designed, I had to map out and categorize the required data into taxonomies and custom post types. Once the backend solutions were finalized, the frontend was built then the website was optimized for SEO and to meet accessibility standards.',
-    url: 'https://www.ln.edu.hk/cultural/',
+      "This project involved building a custom WordPress theme from scratch. Once the UI was designed, I had to map out and categorize the required data into taxonomies and custom post types. Once the backend solutions were finalized, the frontend was built then the website was optimized for SEO and to meet accessibility standards.",
+    url: "https://www.ln.edu.hk/cultural/",
   },
 
   // Pacha
   {
-    id: 'professional7',
-    title: 'Pacha Asia',
+    id: "professional7",
+    title: "Pacha Asia",
     description:
-      'This project was to build a website for an ecommerce company using the Shopify platform to implement a new look for their brand selling environmentally focused feminine care products.',
-    url: 'https://pacha.asia/',
+      "This project was to build a website for an E-commerce company using the Shopify platform to implement a new look for their brand selling environmentally focused feminine care products.",
+    url: "https://pacha.asia/",
   },
 
   // My Foodie Box
   {
-    id: 'professional6',
-    title: 'My Foodie Box',
+    id: "professional6",
+    title: "My Foodie Box",
     description:
-      'This project was to freshen up an existing website by building a new design, also implementing a mobile friendly version.',
-    url: 'https://myfoodiebox.com.au/',
+      "This project was to freshen up an existing website by building a new design, also implementing a mobile friendly version.",
+    url: "https://myfoodiebox.com.au/",
   },
 
   // Jobs In Cyber
   {
-    id: 'professional2',
-    title: 'Jobs In Cyber',
+    id: "professional2",
+    title: "Jobs In Cyber",
     description:
-      'This task was to give a web application a revamp. Carrying out bug fixes, implementing new features and building new designs.',
-    url: '#',
+      "This task was to give a web application a revamp. Carrying out bug fixes, implementing new features and building new designs.",
+    url: "#",
   },
 
   // Enable
@@ -66,10 +66,10 @@ export const professionalProjects = [
 export const personalProjects = [
   // Portfolio site
   {
-    id: 'personal1',
-    title: 'Portfolio site',
+    id: "personal1",
+    title: "Portfolio site",
     description:
-      'For this assignment, I wanted to create my own portfolio website utilizing CSS grid and SCSS along with React Gatsby. I chose to use SCSS as I enjoy using the BEM notation in my HTML - I find this method very clean and clear to read. I also enjoyed steering away from using any CSS framework as I was able to create all my own layouts and components relying on my skills as a CSS developer.',
-    url: 'https://scb-development.vercel.app/',
+      "For this assignment, I wanted to create my own portfolio website utilizing CSS grid and SCSS along with React Gatsby. I chose to use SCSS as I enjoy using the BEM notation in my HTML - I find this method very clean and clear to read. I also enjoyed steering away from using any CSS framework as I was able to create all my own layouts and components relying on my skills as a CSS developer.",
+    url: "https://scb-development.vercel.app/",
   },
 ];
