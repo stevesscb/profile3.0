9b99135649aa589139f0cf779fc4be7ca72f938d
Stephen <stevesscb@gmail.com>
2024-07-01 16:12:13 -0400

    Update about me

--- src/layout/pages/about/About.jsx
+++ src/layout/pages/about/About.jsx
@@ -1,7 +1,7 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import MotionAnimate from 'src/components/MotionAnimate';
-import classes from './about.module.scss';
+import MotionAnimate from "src/components/MotionAnimate";
+import classes from "./about.module.scss";
 
 function About() {
   return (
@@ -9,20 +9,21 @@ function About() {
       <div className={classes.colOne}>
         <MotionAnimate>
           <p>
-            I'm a front end developer - I help bring ideas to life by building
-            designs that clients can proudly stand behind. I enjoy learning new
-            technologies and expanding my skill-set. I'm constantly spending my
-            time researching for more efficient and updated methods.
+            I'm a front end developer, I help bring ideas to life by building
+            out products that users love. I am always learning new technologies
+            and expanding my skills in the tech space. I have worked with a
+            variety of clients ranging from start-ups to educational
+            institutions.
           </p>
           <br />
           <p>
-            While my main experience lies in code, I also have a strong interest
-            in design.
+            Outside of code, I am also strongly interested in design and UX/UI
+            principles.
           </p>
           <br />
           <p>
             If you like what you hear and think we could collaborate please
-            <Link to='/contact' className={classes.cta}>
+            <Link to="/contact" className={classes.cta}>
               &nbsp;get in touch.
             </Link>
           </p>
